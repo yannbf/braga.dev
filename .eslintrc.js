c6fb96622f0a7b36b3ab2a92c173edbd399b922c
Yann Braga <yannbf@gmail.com>
2022-07-23 14:36:21 +0200

    add storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,13 @@ module.exports = {
     node: true,
     es6: true,
   },
-  extends: ['eslint:recommended', 'plugin:prettier/recommended', 'next', 'next/core-web-vitals'],
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'next',
+    'next/core-web-vitals',
+    'plugin:storybook/recommended',
+  ],
   rules: {
     'prettier/prettier': 'error',
     'react/react-in-jsx-scope': 'off',
