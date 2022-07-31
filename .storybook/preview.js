4dfd4813299644b516d6b788e27e0fdeb3d70f76
Yann Braga <yannbf@gmail.com>
2022-07-31 14:21:42 +0200

    update blog post to use rtl mode as an example

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -7,3 +7,20 @@ export const parameters = {
     },
   },
 }
+
+const withRTL = (StoryFn, context) => {
+  const { parameters } = context
+
+  // Set RTL only if passed as a parameter
+  if (parameters.rtl) {
+    return (
+      <div dir="rtl">
+        <StoryFn />
+      </div>
+    )
+  }
+
+  return <StoryFn />
+}
+
+export const decorators = [withRTL]
