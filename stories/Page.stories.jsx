be8d8289cae53dca3c66c5fdff29510b8969f395
Yann Braga <yannbf@gmail.com>
2022-07-31 14:00:30 +0200

    update stories to csf3

--- stories/Page.stories.jsx
+++ stories/Page.stories.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { within, userEvent } from '@storybook/testing-library'
 
 import { Page } from './Page'
@@ -7,19 +6,17 @@ export default {
   title: 'Example/Page',
   component: Page,
   parameters: {
-    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'fullscreen',
+    // rtl: true,
   },
 }
 
-const Template = (args) => <Page {...args} />
+export const LoggedOut = {}
 
-// More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
-export const LoggedOut = Template.bind({})
-
-export const LoggedIn = Template.bind({})
-LoggedIn.play = async ({ canvasElement }) => {
-  const canvas = within(canvasElement)
-  const loginButton = await canvas.getByRole('button', { name: /Log in/i })
-  await userEvent.click(loginButton)
+export const LoggedIn = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    const loginButton = await canvas.getByRole('button', { name: /Log in/i })
+    await userEvent.click(loginButton)
+  },
 }
