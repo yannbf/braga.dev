484bc9662ff9c98a5938017cde372da1bfea39c4
Yann Braga <yannbf@gmail.com>
2022-08-01 21:30:49 +0200

    update config

--- next.config.js
+++ next.config.js
@@ -21,10 +21,10 @@ const securityHeaders = [
     value: ContentSecurityPolicy.replace(/\n/g, ''),
   },
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
-  {
-    key: 'Referrer-Policy',
-    value: 'strict-origin-when-cross-origin',
-  },
+  // {
+  //   key: 'Referrer-Policy',
+  //   value: 'strict-origin-when-cross-origin',
+  // },
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
   // {
   //   key: 'X-Frame-Options',
