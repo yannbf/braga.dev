07d1a6f65757a45000b0e86c3b2d2514bc035d50
Yann Braga <yannbf@gmail.com>
2022-08-07 12:39:50 +0200

    componentize blog article

---
+++ components/Article.js
@@ -0,0 +1,48 @@
+import Link from '@/components/Link'
+import Tag from '@/components/Tag'
+import formatDate from '@/lib/utils/formatDate'
+
+const Article = ({ slug, date, title, summary, tags = [], showReadMore }) => {
+  return (
+    <article>
+      <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+        <dl>
+          <dt className="sr-only">Published on</dt>
+          <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+            <time dateTime={date}>{formatDate(date)}</time>
+          </dd>
+        </dl>
+        <div className="space-y-5 xl:col-span-3">
+          <div className="space-y-6">
+            <div>
+              <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
+                  {title}
+                </Link>
+              </h2>
+              <div className="flex flex-wrap">
+                {tags.map((tag) => (
+                  <Tag key={tag} text={tag} />
+                ))}
+              </div>
+            </div>
+            <div className="prose max-w-none text-gray-500 dark:text-gray-400">{summary}</div>
+          </div>
+          {showReadMore && (
+            <div className="text-base font-medium leading-6">
+              <Link
+                href={`/blog/${slug}`}
+                className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                aria-label={`Read "${title}"`}
+              >
+                Read more &rarr;
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
+    </article>
+  )
+}
+
+export default Article
