a29c3fced271313fa2e6f74fa6fafaf7d72413ed
Yann Braga <yannbf@gmail.com>
2022-08-07 19:33:04 +0200

    improve talks page

---
+++ components/Talk.js
@@ -0,0 +1,54 @@
+import Link from '@/components/Link'
+import Image from '@/components/Image'
+import formatDate from '@/lib/utils/formatDate'
+
+const Talk = ({ link, thumbnail, date, title, description, location }) => {
+  let thumbnailEl = (
+    <Link href={link} title={title} className="w-full xl:w-auto">
+      <Image
+        alt="illustration"
+        className="rounded object-cover"
+        src={`/static/images/talks/${thumbnail}`}
+        layout="responsive"
+        width={640}
+        height={400}
+      />
+    </Link>
+  )
+
+  return (
+    <article className="hover-scale">
+      <div className="grid gap-2 space-y-2 xl:grid-flow-col xl:grid-cols-4 xl:grid-rows-2 xl:gap-4 xl:space-y-0">
+        {thumbnail && <div className="xl:row-span-2">{thumbnailEl}</div>}
+        <div className="order-first space-y-6 xl:order-none xl:col-span-2">
+          <div className="space-y-6">
+            <div>
+              <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                <Link href={link} className="text-gray-900 dark:text-gray-100">
+                  {title}
+                </Link>
+              </h2>
+            </div>
+          </div>
+        </div>
+        <div className="space-y-5 xl:col-span-3">
+          <dl>
+            <dt className="sr-only">Given on</dt>
+            <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+              <time dateTime={date}>
+                {formatDate(date)} ⋅ {location}
+              </time>
+            </dd>
+          </dl>
+          {description && (
+            <div className="xl:col-span-2 xl:row-span-1">
+              <div className="prose max-w-none text-gray-500 dark:text-gray-400">{description}</div>
+            </div>
+          )}
+        </div>
+      </div>
+    </article>
+  )
+}
+
+export default Talk
--- pages/talks.js
+++ pages/talks.js
@@ -1,8 +1,7 @@
 import siteMetadata from '@/data/siteMetadata'
 import talksData from '@/data/talksData'
-import Link from '@/components/Link'
 import { PageSEO } from '@/components/SEO'
-import formatDate from '../lib/utils/formatDate'
+import Talk from '@/components/Talk'
 
 export default function Projects() {
   return (
@@ -13,38 +12,16 @@ export default function Projects() {
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Talks
           </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">Some of my talks</p>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            Some events I've spread the word around
+          </p>
         </div>
         <div className="container py-12">
           <div className="-m-4 flex flex-wrap">
             <ul>
-              {talksData.map((d) => (
-                <li key={d.title} className="py-4">
-                  <article className="space-y-2 space-x-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-                    <dl>
-                      <dt className="sr-only">Given on</dt>
-                      <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                        <time dateTime={d.date}>{formatDate(d.date)}</time>
-                      </dd>
-                    </dl>
-                    <div className="space-y-3 xl:col-span-3">
-                      <div>
-                        <h3 className="text-2xl font-bold leading-8 tracking-tight">
-                          <Link href={d.link} className="text-gray-900 dark:text-gray-100">
-                            {d.title}
-                          </Link>
-                        </h3>
-                        {/* <div className="flex flex-wrap">
-                          {tags.map((tag) => (
-                            <Tag key={tag} text={tag} />
-                          ))}
-                        </div> */}
-                      </div>
-                      <div className="prose max-w-none text-gray-500 dark:text-gray-400">
-                        {d.description}
-                      </div>
-                    </div>
-                  </article>
+              {talksData.map((talk) => (
+                <li key={talk.title} className="py-4">
+                  <Talk {...talk} />
                 </li>
               ))}
             </ul>
