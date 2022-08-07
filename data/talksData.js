9e53dd8c3b904a72290a8ae41bd4e09f1f478133
Yann Braga <yannbf@gmail.com>
2022-08-07 11:23:06 +0200

    add more talks

--- data/talksData.js
+++ data/talksData.js
@@ -11,6 +11,24 @@ const talksData = [
     description: `Angular Nation`,
     link: 'https://www.youtube.com/watch?v=mbrQYL8FHWM',
   },
+  {
+    title: 'New Zeplin Storybook integration',
+    date: '11-15-2021',
+    description: `Zeplin`,
+    link: 'https://www.youtube.com/watch?v=YGQSvf2yNQ8',
+  },
+  {
+    title: 'Smooth design handoff with Storybook',
+    date: '03-05-2021',
+    description: `iO technology`,
+    link: 'https://www.youtube.com/watch?v=2LBBrIulDe0',
+  },
+  {
+    title: 'Monorepo: uma história de sucesso (Portuguese)',
+    date: '07-09-2020',
+    description: `BrazilJS`,
+    link: 'https://www.youtube.com/watch?v=2lvPT4VyfqE',
+  },
 ]
 
 export default talksData
