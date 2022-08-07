cd9d7f6c4508b7f6407bd846a3c35de2bfaf0d4e
Yann Braga <yannbf@gmail.com>
2022-08-07 11:40:36 +0200

    add border to theme switcher

--- components/ThemeSwitch.js
+++ components/ThemeSwitch.js
@@ -12,7 +12,7 @@ const ThemeSwitch = () => {
     <button
       aria-label="Toggle Dark Mode"
       type="button"
-      className="ml-1 mr-1 h-8 w-8 rounded p-1 sm:ml-4"
+      className="ml-1 h-8 w-8 rounded p-1 hover:bg-gray-200 dark:hover:bg-gray-700 sm:ml-2"
       onClick={() => setTheme(theme === 'dark' || resolvedTheme === 'dark' ? 'light' : 'dark')}
     >
       <svg
