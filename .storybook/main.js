87dccd2e44522d255bf359e8d20e0dd67c85c9c0
Yann Braga <yannbf@gmail.com>
2022-08-13 11:23:52 +0200

    improve storybook for local dev

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,17 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    // Only show demo stories in published storybook
+    {
+      directory: process.env.NODE_ENV === 'development' ? '../' : '../stories',
+    },
+  ],
+  staticDirs: ['../public'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     'storybook-addon-variants',
+    'storybook-addon-next',
   ],
   framework: {
     name: '@storybook/react-webpack5',
