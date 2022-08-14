8ca8bf519d86655012be7731d43c525c37d5252b
Yann Braga <yannbf@gmail.com>
2022-08-14 16:59:32 +0200

    small adjustments

--- components/Talk.js
+++ components/Talk.js
@@ -15,14 +15,14 @@ const Talk = ({ link, thumbnail, title, tags, location }) => {
 
   return (
     <Link href={link} title={title} className="hover-scale">
-      <div className="border-1 h-full overflow-hidden rounded-2xl border-gray-200 border-opacity-20 bg-white shadow-lg shadow-gray-400/20 dark:bg-gray-800 dark:shadow-gray-800/20">
+      <div className="border-1 h-full overflow-hidden rounded-2xl border-gray-200 border-opacity-20 bg-white shadow-lg shadow-gray-400/20 dark:bg-[#303030] dark:shadow-gray-800/20">
         {thumbnailEl}
 
         <div className="p-4">
           <div className="title-font mb-1 text-xs font-bold uppercase tracking-widest text-primary-400">
             {tags.map((t) => '#' + t).join(' ')}
           </div>
-          <h2 className="title-font mb-3 text-lg font-medium text-gray-900 dark:text-gray-100">
+          <h2 className="title-font mb-3 text-xl font-medium text-gray-900 dark:text-gray-100">
             {title}
           </h2>
           <div className="flex flex-wrap items-center ">
