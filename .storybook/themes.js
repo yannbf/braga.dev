3d056ecf99cf0c19b3d8afde64fd0f5b326874ca
Yann Braga <yannbf@gmail.com>
2022-08-19 01:25:55 +0200

    update colors

--- .storybook/themes.js
+++ .storybook/themes.js
@@ -10,7 +10,7 @@ export const appThemes = {
     text: '#fff',
     backgrounds: {
       app: '#000',
-      card: 'linear-gradient(to bottom right, #673ab7, #4a4a4a)',
+      card: 'linear-gradient(to bottom right, #252525, #4a4a4a)',
     },
   },
   acqua: {
